d23bd09677375539ce0b2c70b469f41d980f41cf
ingvarr777 <47151220+ingvarr777@users.noreply.github.com>
2020-05-04 22:11:52 +0300

    Added cardList, refactored index.js

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,7 @@ const Card = ({ name, email, id }) => {
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 			<img alt='Friendly robot face' src={`https://robohash.org/${id}.png?size=200x200`}/>
 			<div>
-				<h2>{name}</h2>
+				<h2 className = 'f5'>{name}</h2>
 				<p>{email}</p>
 			
 			</div>
