c0f99695d1d92a20812731fd76a1abec315c9882
ingvarr777 <47151220+ingvarr777@users.noreply.github.com>
2020-05-05 21:55:56 +0300

    Added styles; added fetching userdata from JSONPlaceholder; added loading message

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
-import { robots } from './robots';
-
+// import { robots } from './robots';
+import './App.css';
 
 
 class App extends Component {
 	constructor () {
 		super ()
 		this.state = {
-			robots: robots,
+			robots:[],
 			searchfield: ''
 		}
 		
 	}
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({robots: users}))
+		
+	}
+
 	onSearchChange = (event) => {
 		this.setState({searchfield: event.target.value})		
 		
@@ -22,9 +29,12 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robots => {
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 		})
+		if (this.state.robots.length === 0) {
+			return <h1>Loading</h1>
+		}
 		return (		
 			<div className='tc'>
-				<h1>RoboFriends</h1>
+				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange} />
 				<CardList robots={ filteredRobots } />
 			</div>		
