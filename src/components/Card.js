d38fd137ef9ad706eafd9ad10bab463e3c7ebc4a
ingvarr777 <47151220+ingvarr777@users.noreply.github.com>
2020-05-06 01:00:44 +0300

    Optimized font size

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,8 @@ const Card = ({ name, email, id }) => {
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 			<img alt='Friendly robot face' src={`https://robohash.org/${id}.png?size=200x200`}/>
 			<div>
-				<h2 className = 'f5'>{name}</h2>
-				<p>{email}</p>			
+				<h2 className = 'f6'>{name}</h2>
+				<p className = 'f6'>{email}</p>			
 			</div>
 		</div>
 	)
